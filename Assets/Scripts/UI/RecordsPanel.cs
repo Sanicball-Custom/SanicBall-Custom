772ed8edd750059505a580cc3e174284ecf7c31d
KeyKoder <victor.fernandez.montano@gmail.com>
2021-05-06 17:57:22 +0200

    Revamp records panel

--- Assets/Scripts/UI/RecordsPanel.cs
+++ Assets/Scripts/UI/RecordsPanel.cs
@@ -10,6 +10,11 @@ namespace Sanicball.UI
     public class RecordsPanel : MonoBehaviour
     {
         public Text stageNameField;
+        public Text individualCharactersField;
+
+        public Transform tierRecords;
+        public Transform characterRecords;
+
         public RecordTypeControl lapRecord;
         public RecordTypeControl hyperspeedLapRecord;
 
@@ -17,9 +22,16 @@ namespace Sanicball.UI
 		public RectTransform sectionHeaderPrefab;
 		public RectTransform recordTypeContainer; 
 
-		private List<RecordTypeControl> recordTypes = new List<RecordTypeControl> ();
+		private List<RecordTypeControl> recordTypesByTier = new List<RecordTypeControl> ();
+        private List<RecordTypeControl> recordTypesByCharacter = new List<RecordTypeControl>();
+
 
         private int selectedStage = 0;
+        private bool individualCharacters = false;
+        public void IndividualCharactersToggle() {
+            individualCharacters = !individualCharacters;
+            UpdateIndividualCharacters();
+        }
 
         public void IncrementStage()
         {
@@ -45,18 +57,30 @@ namespace Sanicball.UI
         {
 			foreach (CharacterTier tier in System.Enum.GetValues (typeof(CharacterTier)).Cast<CharacterTier>())
 			{
-				RectTransform header = Instantiate(sectionHeaderPrefab);
-				header.GetComponentInChildren<Text>().text = tier.ToString () + " balls"; //yes
-				header.SetParent (recordTypeContainer, false);
+				RectTransform tierHeader = Instantiate(sectionHeaderPrefab);
+				tierHeader.GetComponentInChildren<Text>().text = tier.ToString () + " balls"; //yes
+				tierHeader.SetParent (tierRecords, false);
 
-				RecordTypeControl ctrl = Instantiate (recordTypeControlPrefab);
-				ctrl.transform.SetParent (recordTypeContainer, false);
+                //RectTransform characterHeader = Instantiate(sectionHeaderPrefab);
+                //characterHeader.GetComponentInChildren<Text>().text = tier.ToString() + " balls"; //yes
+                //characterHeader.SetParent(characterRecords, false);
+
+                RecordTypeControl ctrl = Instantiate (recordTypeControlPrefab);
+				ctrl.transform.SetParent (tierRecords, false);
 				ctrl.titleField.text = "Lap record";
-				recordTypes.Add (ctrl);
-			}
+				recordTypesByTier.Add(ctrl);
+            }
+
+            foreach (Sanicball.Data.CharacterInfo charinfo in ActiveData.Characters) {
+                RecordTypeControl charCtrl = Instantiate(recordTypeControlPrefab);
+                charCtrl.transform.SetParent(characterRecords, false);
+                charCtrl.titleField.text = charinfo.name + " lap record";
+                recordTypesByCharacter.Add(charCtrl);
+            }
 
             UpdateFields();
             UpdateStageName();
+            UpdateIndividualCharacters();
         }
 
         private void UpdateFields()
@@ -64,22 +88,40 @@ namespace Sanicball.UI
             var stageindex = ActiveData.Stages[selectedStage].id;
             var records = ActiveData.RaceRecords.Where(a => a.Stage == stageindex && a.GameVersion == GameVersion.AS_FLOAT && a.WasTesting == GameVersion.IS_TESTING).OrderBy(a => a.Time);
 
-			for (int i = 0; i < recordTypes.Count(); i++) {
-				var ctrl = recordTypes[i];
-				var bestLapRecord = records.Where(a => a.Tier == (CharacterTier)i).FirstOrDefault();
-				ctrl.SetRecord(bestLapRecord);
-			}
+            if (!individualCharacters) {
+                tierRecords.gameObject.SetActive(true);
+                characterRecords.gameObject.SetActive(false);
+                for (int i = 0; i < recordTypesByTier.Count(); i++) {
+                    var ctrl = recordTypesByTier[i];
+                    var bestLapRecord = records.Where(a => a.Tier == (CharacterTier)i).FirstOrDefault();
+                    ctrl.SetRecord(bestLapRecord);
+                }
+            } else {
+                tierRecords.gameObject.SetActive(false);
+                characterRecords.gameObject.SetActive(true);
+                for (int i = 0; i < recordTypesByCharacter.Count(); i++) {
+                    var ctrl = recordTypesByCharacter[i];
+                    var bestLapRecord = records.Where(a => a.Character == i).FirstOrDefault();
+                    ctrl.gameObject.SetActive(bestLapRecord != null);
+                    ctrl.SetRecord(bestLapRecord);
+                }
+            }
 
-//            var bestLapRecord = records.Where(a => a.Type == RecordType.Lap).FirstOrDefault();
-//            lapRecord.SetRecord(bestLapRecord);
-//
-//            var bestHyperspeedLapRecord = records.Where(a => a.Type == RecordType.HyperspeedLap).FirstOrDefault();
-//            hyperspeedLapRecord.SetRecord(bestHyperspeedLapRecord);
+            //            var bestLapRecord = records.Where(a => a.Type == RecordType.Lap).FirstOrDefault();
+            //            lapRecord.SetRecord(bestLapRecord);
+            //
+            //            var bestHyperspeedLapRecord = records.Where(a => a.Type == RecordType.HyperspeedLap).FirstOrDefault();
+            //            hyperspeedLapRecord.SetRecord(bestHyperspeedLapRecord);
         }
 
         private void UpdateStageName()
         {
             stageNameField.text = ActiveData.Stages[selectedStage].name;
+            UpdateFields();
+        }
+
+        private void UpdateIndividualCharacters() {
+            individualCharactersField.text = individualCharacters ? "Yes" : "No";
             UpdateFields();
         }
     }
